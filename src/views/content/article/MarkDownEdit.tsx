4ff6cafe388e56282672e0803363242e62835cab
fishworm96 <245525860@qq.com>
2023-03-05 20:58:27 +0800

    feat: 添加剪贴板上传图片

--- src/views/content/article/MarkDownEdit.tsx
+++ src/views/content/article/MarkDownEdit.tsx
@@ -1,52 +1,74 @@
-import { useCallback, useState } from "react";
-import { createEditor, Editor, Transforms } from "slate";
-import { Editable, Slate, withReact } from "slate-react";
+import React, { useEffect, useRef, useState } from "react";
+import MdEditor, { ExposeParam } from "md-editor-rt";
+import "md-editor-rt/lib/style.css";
 
-const MarkDownEdit = () => {
-	const [editor] = useState(() => withReact(createEditor()));
+import "./markdown.less";
+import axios from "axios";
 
-	const initialValue = [
-		{
-			type: "code",
-			children: [{ text: "A line of text in a paragraph." }]
-		}
-	];
+const MarkDownEdit = ({ content }: { content: string }) => {
+	const [text, setText] = useState("");
+	const editorRef = useRef<ExposeParam>();
 
-	const renderElement = useCallback((props: any) => {
-		switch (props.element.type) {
-			case "code":
-				return <CodeElement {...props} />;
-			default:
-				return <DefaultElement {...props} />;
-		}
+	useEffect(() => {
+		editorRef.current?.on("catalog", console.log);
+		upLoadImage();
+		setText(content);
 	}, []);
 
-	return (
-		<Slate editor={editor} value={initialValue}>
-			<Editable
-				renderElement={renderElement}
-				onKeyDown={event => {
-					if (event.key === "`" && event.ctrlKey) {
-						// Prevent the "`" from being inserted by default.
-						event.preventDefault();
-						// Otherwise, set the currently selected blocks type to "code".
-						Transforms.setNodes(editor, { type: "code" }, { match: n => Editor.isBlock(editor, n) });
-					}
-				}}
-			/>
-		</Slate>
-	);
+	// 剪贴板上传图片
+	const upLoadImage = () => {
+		const textarea = document.getElementById("md-editor-rt-textarea");
+		textarea?.addEventListener("paste", e => {
+			// 获取文本值
+			// e.clipboardData?.getData("text");
+			// 获取图片
+			// 读取到图片对象
+			let file = e.clipboardData?.items[0].getAsFile();
+			// 判断类型
+			if (!file || !/^image\/(jpeg|png|gif|jpg)$/.test(file!.type)) {
+				return;
+			}
+			editorRef.current?.insert(file => {
+				/**
+				 * @return targetValue    待插入内容
+				 * @return select         插入后是否自动选中内容
+				 * @return deviationStart 插入后选中内容鼠标开始位置
+				 * @return deviationEnd   插入后选中内容鼠标结束位置
+				 */
+				return {
+					targetValue: `${file}`,
+					select: true,
+					deviationStart: 0,
+					deviationEnd: 0
+				};
+			});
+		});
+	};
+
+	const onUploadImg = async (files: Array<File>, callback: (urls: Array<string>) => void) => {
+		console.log(files, callback);
+		const res = await Promise.all(
+			files.map(file => {
+				return new Promise((rev, rej) => {
+					const form = new FormData();
+					form.append("file", file);
+
+					axios
+						.post("/api/img/uploads", form, {
+							headers: {
+								"Content-Type": "multipart/form-data"
+							}
+						})
+						.then(res => rev(res))
+						.catch(error => rej(error));
+				});
+			})
+		);
+
+		callback(res.map(item => item.data.url));
+	};
+
+	return <MdEditor ref={editorRef} style={{ height: 700 }} modelValue={text} onChange={setText} onUploadImg={onUploadImg} />;
 };
 
-const CodeElement = (props: any) => {
-	return (
-		<pre {...props.attributes}>
-			<code>{props.children}</code>
-		</pre>
-	);
-};
-
-const DefaultElement = (props: any) => {
-	return <p {...props.attributes}>{props.children}</p>;
-};
 export default MarkDownEdit;
--- src/views/content/article/option.tsx
+++ src/views/content/article/option.tsx
@@ -1,46 +1,50 @@
-import { Form, Input, Modal } from "antd";
-import { useNavigate } from "react-router-dom";
-import MarkDownEdit from "./markDownEdit";
+import { Form, Input, Select } from "antd";
+import MarkDownEdit from "./MarkdownEdit";
+import type { SelectProps } from "antd";
 
 const Option = () => {
-	const navigate = useNavigate();
+	// const navigate = useNavigate();
 
-	const onSubmit = () => {
-		navigate("/content/article");
-	};
+	// const onSubmit = () => {
+	// 	navigate("/content/article");
+	// };
 
-	const onCancel = () => {
-		navigate(-1);
-	};
+	// const onCancel = () => {
+	// 	navigate(-1);
+	// };
 
 	// const handleEditorChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
 	// 	setEdit(e.target.value);
 	// };
 
+	const options: SelectProps["options"] = [{ label: 1, value: 1 }];
+
+	const handleChange = (value: string[]) => {
+		console.log(`selected ${value}`);
+	};
+
 	return (
 		<>
-			<Modal title="Modal 1000px width" centered open={true} onOk={onSubmit} onCancel={onCancel} width={1800}>
-				<div style={{ display: "flex", alignContent: "space-between" }}>
-					<Form style={{ marginRight: 50 }}>
-						<Form.Item label="标题" name="title" rules={[{ required: true, message: "Please input your username!" }]}>
-							<Input />
-						</Form.Item>
-						<Form.Item label="简述" name="describe" rules={[{ required: true, message: "Please input your username!" }]}>
-							<Input />
-						</Form.Item>
-						<Form.Item label="标签" name="tag" rules={[{ required: true, message: "Please input your username!" }]}>
-							<Input />
-						</Form.Item>
-						{/* <Form.Item
-							style={{ width: 800 }}
-							label="内容"
-							name="content"
-							rules={[{ required: true, message: "Please input your username!" }]}
-						></Form.Item> */}
-						<MarkDownEdit />
-					</Form>
-				</div>
-			</Modal>
+			<Form style={{ marginRight: 50 }}>
+				<Form.Item label="标题" name="title" rules={[{ required: true, message: "Please input your username!" }]}>
+					<Input />
+				</Form.Item>
+				<Form.Item label="简述" name="describe" rules={[{ required: true, message: "Please input your username!" }]}>
+					<Input />
+				</Form.Item>
+				<Form.Item label="标签" name="tag" rules={[{ required: true, message: "请选择标签" }]}>
+					<Select
+						mode="multiple"
+						allowClear
+						style={{ width: "100%" }}
+						placeholder="请选择标签"
+						// defaultValue={["a10", "c12"]}
+						onChange={handleChange}
+						options={options}
+					/>
+				</Form.Item>
+				<MarkDownEdit content={"# 1"} />
+			</Form>
 		</>
 	);
 };
